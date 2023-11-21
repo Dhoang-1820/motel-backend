8d024793c6949a5c6067f62fae90ad18ee1a5676
Dhoang-1820 <doh7118@gmail.com>
2023-11-21 23:21:16 +0700

    build

--- src/main/java/com/petproject/motelservice/starter/MotelServiceApplication.java
+++ src/main/java/com/petproject/motelservice/starter/MotelServiceApplication.java
@@ -2,6 +2,7 @@ package com.petproject.motelservice.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-public class MotelServiceApplication {
+public class MotelServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MotelServiceApplication.class, args);
